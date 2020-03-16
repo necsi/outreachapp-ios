8e5a33cdfc03afc9731d02c2ed2c7776975b74c1
bejitono <stdemicheli@protonmail.com>
2020-03-16 21:17:56 +0700

    Implements add notificaiton view model.

---
+++ OutreachApp/Module/MemberNotification/ViewModel/AddMemberNotificationViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  AddMemberNotificationViewModel.swift
+//  OutreachApp
+//
+//  Created by Demicheli, Stefano on 16/3/2563 BE.
+//  Copyright © 2563 NECSI. All rights reserved.
+//
+
+struct AddMemberNotificationViewModel {
+
+    var addNotificationTitle: String {
+        // TODO: Localize
+        return "Add notification"
+    }
+
+    var noteTextFieldTitle: String {
+        // TODO: Localize
+        return "NOTE"
+    }
+
+    var doneButtonTitle: String {
+        // TODO: Localize
+        return "Done"
+    }
+}
