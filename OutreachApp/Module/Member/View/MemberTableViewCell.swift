ca5edb619d32f8ef39ebe47623f8598af7d7cac2
bejitono <stdemicheli@protonmail.com>
2020-03-17 11:45:03 +0700

    Implements member cell view delegate.

--- OutreachApp/Module/Member/View/MemberTableViewCell.swift
+++ OutreachApp/Module/Member/View/MemberTableViewCell.swift
@@ -8,14 +8,33 @@
 
 import UIKit
 
+protocol MemberTableViewCellDelegate: class {
+
+    func didTapAccessoryView(at indexPath: IndexPath)
+}
+
 final class MemberTableViewCell: UITableViewCell {
 
     static let reuseId = "MemberTableViewCell"
+    var indexPath: IndexPath?
+    weak var delegate: MemberTableViewCellDelegate?
 
-    func configure(with viewModel: MemberCellViewModel) {
+    func configure(with viewModel: MemberCellViewModel, indexPath: IndexPath) {
         textLabel?.text = viewModel.fullName
         textLabel?.textColor = Theme.Color.primaryText
         selectionStyle = .none
         backgroundColor = Theme.Color.background
+        self.indexPath = indexPath
+        let button = UIButton(type: .contactAdd)
+        button.addTarget(self, action: #selector(didTapAccessoryView), for: .touchUpInside)
+        accessoryView = button
+    }
+
+    @objc func didTapAccessoryView() {
+        guard let indexPath = indexPath else {
+            assertionFailure("Table view did not provide index path.")
+            return
+        }
+        delegate?.didTapAccessoryView(at: indexPath)
     }
 }
