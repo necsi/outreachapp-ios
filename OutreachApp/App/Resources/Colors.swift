b8b4c93807a0830ba5bb4e9d07fc6a4237ec4f47
bejitono <stdemicheli@protonmail.com>
2020-03-17 09:40:22 +0700

    Adds colors.

---
+++ OutreachApp/App/Resources/Colors.swift
@@ -0,0 +1,22 @@
+//
+//  Colors.swift
+//  OutreachApp
+//
+//  Created by Demicheli, Stefano on 17/3/2563 BE.
+//  Copyright © 2563 NECSI. All rights reserved.
+//
+
+import UIKit
+
+struct Theme {
+
+    struct Color {
+
+        static let action = UIColor(named: "action")!
+        static let background = UIColor(named: "background")!
+        static let secondaryBackground = UIColor(named: "secondaryBackground")!
+        static let primaryText = UIColor(named: "primaryText")!
+        static let secondaryText = UIColor(named: "secondaryText")!
+        static let attention = UIColor(named: "attention")!
+    }
+}
